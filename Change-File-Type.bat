01b71e9a7750779f99070f45d0c963682b46cc72
Jacob McShane <jimster6543@gmail.com>
2025-12-19 18:33:47 -0500

    Update Change-File-Type.bat

--- Change-File-Type.bat
+++ Change-File-Type.bat
@@ -1,14 +1,35 @@
 @echo off
+setlocal enabledelayedexpansion
 
-set /p oldFileType=Enter the file type you want to change:
-set /p newFileType=Enter the new file type:
-set /p directory=Enter the directory where the files are located:
+echo Enter the old file extension (e.g., txt - without dot):
+set /p "oldExt=>"
 
-cd /d %directory%
+echo Enter the new file extension (e.g., jpg - without dot):
+set /p "newExt=>"
 
-for /f "delims=" %%i in ('dir /b *%oldFileType%') do (
-ren "%%i" "%%~ni.%newFileType%"
+echo Enter the directory path (or press Enter for current directory):
+set /p "dirPath=>"
+
+if "%dirPath%"=="" set "dirPath=."
+
+pushd "%dirPath%" 2>nul
+if errorlevel 1 (
+    echo Error: Directory not found or inaccessible.
+    pause
+    exit /b 1
 )
 
-echo File type conversion complete.
+set count=0
+
+for %%i in (*.%oldExt%) do (
+    ren "%%i" "%%~ni.%newExt%"
+    set /a count+=1
+)
+
+echo Renamed !count! file(s) from .%oldExt% to .%newExt%.
+
+if !count! == 0 echo No files with extension .%oldExt% found.
+
+popd
+echo Operation complete.
 pause
